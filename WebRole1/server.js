51e29ce514ffe1cf5b5c5675bd0271e230072b42
daddyo <jburnett@altamodatech.com>
2012-08-07 23:13:57 -0400

    Dygraph samples working

---
+++ WebRole1/server.js
@@ -0,0 +1,42 @@
+
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+ 
+// var webroot = './public';
+var port = process.env.PORT || 80;
+
+var server = http.createServer(function (request, response) {
+ 
+    // console.log('request starting...');
+	
+	var filePath = '.' + request.url;
+	if (filePath == './') {
+		filePath = './default.html';
+	}
+
+	path.exists(filePath, function(exists) {
+		if (exists) {
+			fs.readFile(filePath, function(error, content) {
+				if (error) {
+					response.writeHead(500);
+					response.end();
+				}
+				else {
+					response.writeHead(200, { 'Content-Type': 'text/html' });
+					response.end(content, 'utf-8');
+				}
+			});
+		}
+		else {
+			// TODO: uncomment for PROD
+			response.writeHead(404);
+			response.end();
+		}
+	});
+	
+});
+
+server.listen( port );
+ 
+console.log('Server running on port ' + port );
