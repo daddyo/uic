63c3ab6572e8a9c9b2d37dcfdae39f5dc06d0249
daddyo <jburnett@altamodatech.com>
2012-08-22 18:01:28 -0400

    Displaying metrics from local json

--- node/config/routes.js
+++ node/config/routes.js
@@ -18,6 +18,26 @@ console.log('local filepath: ' + filePath);
         // });
     });
 
+    
+    app.get('/data/capacity', function(req, res) {
+        var filePath = path.join( __dirname, '../testdata/MetricsCapacityBlob.json' );
+        console.log("Sending json data from " + filePath);
+        fs.readFile(filePath, function(err, content) {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(content, 'utf-8'); 
+        });
+    });
+
+    app.get('/data/transactions', function(req, res) {
+        var filePath = path.join( __dirname, '../testdata/MetricsTransactionsBlob.json' );
+        console.log("Sending json data from " + filePath);
+        fs.readFile(filePath, function(err, content) {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(content, 'utf-8'); 
+        });
+    });
+
+
     app.get('/about', function(req, res) {
         res.render('about/about', {title: 'About' } );
     });
